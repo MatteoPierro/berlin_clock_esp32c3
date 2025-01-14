cbf7fa2e8a9b330bc2c5e2f4ee914d15bdf48b30
Matteo Pierro <pierromatteo@gmail.com>
2025-01-14 16:43:10 +0200

    toggle led

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,16 +1,25 @@
-use std::time::SystemTime;
-use chrono::{Local, Timelike};
+use chrono::Local;
+use esp_idf_svc::hal::delay::FreeRtos;
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::peripherals::Peripherals;
 use berlin_clock::add;
 
 fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
-
-    // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let now = Local::now();
     log::info!("Hello, {}", now);
     log::info!("Adding {}", add(2, 3));
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = PinDriver::input_output(peripherals.pins.gpio7).unwrap();
+
+    loop {
+        led.set_high().unwrap();
+        log::info!("level {:?}", &led.get_level());
+        FreeRtos::delay_ms(1000);
+        led.set_low().unwrap();
+        log::info!("level {:?}", &led.get_level());
+        FreeRtos::delay_ms(3000);
+    }
 }
