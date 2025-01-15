4d508482bdfc64aa88bf750182c1a1b8e1867359
Matteo Pierro <pierromatteo@gmail.com>
2025-01-15 22:27:31 +0200

    move connection to lib

--- app/src/bin/wifi.rs
+++ app/src/bin/wifi.rs
@@ -1,14 +1,10 @@
+use berlin_clock_hardware::fetch_time;
 use chrono::Local;
-use core::convert::TryInto;
 use esp_idf_svc::hal::prelude::Peripherals;
 use esp_idf_svc::log::EspLogger;
-use esp_idf_svc::wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi};
-use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, sntp};
-use esp_idf_svc::hal::modem::Modem;
 use log::info;
-
-const SSID: &str = env!("SSID");
-const PASSWORD: &str = env!("PASSWORD");
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() -> anyhow::Result<()> {
     EspLogger::initialize_default();
@@ -16,49 +12,7 @@ fn main() -> anyhow::Result<()> {
     fetch_time(peripherals.modem)?;
 
     loop {
-        // To get a better formatting of the time, you can use the `chrono` or `time` Rust crates
         info!("Current time: {:?}", Local::now());
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        sleep(Duration::from_secs(1));
     }
-}
-
-fn fetch_time(modem: Modem) ->  anyhow::Result<()> {
-    let sys_loop = EspSystemEventLoop::take()?;
-    let nvs = EspDefaultNvsPartition::take()?;
-
-    let mut wifi = BlockingWifi::wrap(
-        EspWifi::new(modem, sys_loop.clone(), Some(nvs)).unwrap(),
-        sys_loop,
-    )?;
-
-    connect_wifi(&mut wifi)?;
-
-    // Keep it around or else the SNTP service will stop
-    let _sntp = sntp::EspSntp::new_default()?;
-    info!("SNTP initialized");
-    Ok(())
-}
-
-fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
-    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
-        ssid: SSID.try_into().unwrap(),
-        bssid: None,
-        auth_method: AuthMethod::WPA2Personal,
-        password: PASSWORD.try_into().unwrap(),
-        channel: None,
-        ..Default::default()
-    });
-
-    wifi.set_configuration(&wifi_configuration)?;
-
-    wifi.start()?;
-    info!("Wifi started");
-
-    wifi.connect()?;
-    info!("Wifi connected");
-
-    wifi.wait_netif_up()?;
-    info!("Wifi netif up");
-
-    Ok(())
 }
--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,5 +1,11 @@
+use esp_idf_svc::eventloop::EspSystemEventLoop;
 use berlin_clock::LightState;
 use esp_idf_svc::hal::gpio::{Gpio0, Gpio1, Gpio10, Gpio2, Gpio3, Gpio4, Gpio5, Gpio6, Gpio7, Gpio8, Gpio9, InputOutput, Pin, PinDriver};
+use esp_idf_svc::hal::modem::Modem;
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::sntp;
+use esp_idf_svc::wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi};
+use log::info;
 
 pub struct ClockPins<'a> {
     // pub seconds: PinDriver<'a, Gpio0, InputOutput>,
@@ -78,4 +84,48 @@ fn toggle<T: Pin>(one: &mut PinDriver<T, InputOutput>, value: LightState) {
     } else {
         one.set_low().unwrap();
     }
+}
+
+const SSID: &str = env!("SSID");
+const PASSWORD: &str = env!("PASSWORD");
+
+pub fn fetch_time(modem: Modem) ->  anyhow::Result<()> {
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(modem, sys_loop.clone(), Some(nvs)).unwrap(),
+        sys_loop,
+    )?;
+
+    connect_wifi(&mut wifi)?;
+
+    // Keep it around or else the SNTP service will stop
+    let _sntp = sntp::EspSntp::new_default()?;
+    info!("SNTP initialized");
+    Ok(())
+}
+
+fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: PASSWORD.try_into().unwrap(),
+        channel: None,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start()?;
+    info!("Wifi started");
+
+    wifi.connect()?;
+    info!("Wifi connected");
+
+    wifi.wait_netif_up()?;
+    info!("Wifi netif up");
+
+    Ok(())
 }
