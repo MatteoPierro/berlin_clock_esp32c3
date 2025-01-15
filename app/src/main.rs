c8c231960ff71de4ae1685e8ead6188883c90f84
Matteo Pierro <pierromatteo@gmail.com>
2025-01-15 17:34:06 +0200

    smoke test

--- app/src/main.rs
+++ app/src/main.rs
@@ -12,14 +12,19 @@ fn main() {
     log::info!("Adding {}", add(2, 3));
 
     let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::input_output(peripherals.pins.gpio7).unwrap();
+    let mut zero = PinDriver::input_output(peripherals.pins.gpio0).unwrap();
+    let mut one = PinDriver::input_output(peripherals.pins.gpio1).unwrap();
+    let mut two = PinDriver::input_output(peripherals.pins.gpio2).unwrap();
+    let mut three = PinDriver::input_output(peripherals.pins.gpio3).unwrap();
+    let mut four = PinDriver::input_output(peripherals.pins.gpio4).unwrap();
 
     loop {
-        led.set_high().unwrap();
-        log::info!("level {:?}", &led.get_level());
+        zero.toggle().unwrap();
+        one.toggle().unwrap();
+        two.toggle().unwrap();
+        three.toggle().unwrap();
+        four.toggle().unwrap();
+        log::info!("level {:?}", &zero.get_level());
         FreeRtos::delay_ms(1000);
-        led.set_low().unwrap();
-        log::info!("level {:?}", &led.get_level());
-        FreeRtos::delay_ms(3000);
     }
 }
